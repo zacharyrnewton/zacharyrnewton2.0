982bf2b0a05fd9a48beb8f3e61bf55cf57b77475
zacharyrnewton <zacharyrnewton@gmail.com>
2017-03-21 18:23:55 -0400

    added smooth scroll

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,3 +5,18 @@ $('.toggled-nav-wrapper').on('click', function(e){
 $('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
   $('html').toggleClass('nav-open');
 });
+//Smooth Scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
